96806b054b568fb71bdf8e5cc22729794ecb5ee6
Lukas Leisten <65678882+Tynopia@users.noreply.github.com>
2023-11-29 23:25:10 +0100

    feat: Initial Tebex Headless Code

--- README.md
+++ README.md
@@ -1,1 +1,99 @@
-# tebex_headless
+# Tebex Headless API Wrapper
+
+![Tebex Headless](https://www.gitbook.com/cdn-cgi/image/width=256,dpr=2,height=40,fit=contain,format=auto/https%3A%2F%2F1785315218-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FaRz9HUAxrYuZsSDWhYnu%252Flogo%252FSSLScS9oAZjiUBkr89EK%252FOW_Dev_Logo_desktop%25402x.png%3Falt%3Dmedia%26token%3Ddcd44482-188c-4d07-bafe-a8f714fbc691)
+
+Welcome to the Tebex Headless API Wrapper! This Node.js TypeScript library allows you to seamlessly integrate Tebex into your projects and build a custom storefront without revealing your use of Tebex.
+
+## Installation
+
+```bash
+npm install tebex_headless
+```
+
+## Getting Started
+
+1. Obtain your Tebex Headless API public token from your Tebex account.
+2. Install the `tebex_headless` library in your project.
+3. Start using the Tebex Headless API in your Node.js TypeScript application!
+
+```typescript
+import { SetWebstoreIdentifier } from "tebex_headless";
+
+SetWebstoreIdentifier("your_public_token")
+```
+
+## Functions
+
+```typescript
+// Get all categories of the webstore
+async GetCategories(includePackages?: boolean, basketIdent?: string, ipAddress?: string)
+
+// Get a specific category by an id
+async GetCategory(id: number, includePackages?: boolean, basketIdent?: string, ipAddress?: string)
+
+// Apply a coupon, gift card, or creator code
+async Apply<T>(t: T, basketIdent: string, type: ApplyType)
+
+// Remove a coupon, gift card, or creator code
+async Remove<T>(t: T, basketIdent: string, type: ApplyType)
+
+// Get a specific package by an id inside a basket
+async GetPackage(id: number, basketIdent?: string, ipAddress?: string)
+
+// Get all packages of the webstore inside a basket
+async GetPackages(basketIdent?: string, ipAddress?: string)
+
+// Get a basket by an identifier
+async GetBasket(basketIdent: string)
+
+// Create a basket and provide complete and cancel URLs
+async CreateBasket(urls: Urls)
+
+// Get all auth URLs by a basket
+async GetBasketAuthUrl(basketIdent: string, returnUrl: string)
+
+// Add a package to a specific basket
+async AddPackageToBasket(basketIdent: string, package_id: number, quantity: number, type: PackageType)
+
+// Gift a package to a target user
+async GiftPackage(basketIdent: string, package_id: number, target_username_id: string)
+
+// Remove a package inside a specific basket and package_id
+async RemovePackage(basketIdent: string, package_id: number)
+
+// Update the quantity of a package inside a basket
+async UpdateQuantity(basketIdent: string, package_id: number, quantity: number)
+
+// Get webstore information by the public token
+async GetWebstore()
+```
+
+## Features
+
+- **Seamless Integration:** Use Tebex functionalities in your project without exposing the fact that you are using Tebex.
+- **Custom Storefront:** Build your custom storefront by leveraging Tebex Headless API.
+
+## Contributing
+
+We welcome contributions from the community! If you'd like to contribute to this project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and ensure tests pass.
+4. Submit a pull request with a clear description of your changes.
+
+## Documentation
+
+Check out the [official Tebex documentation](https://docs.tebex.io/) for detailed information about the Tebex Headless API and its features.
+
+## Support
+
+If you have questions or need assistance, feel free to [open an issue](https://github.com/grp-gg/tebex_headless/issues) on the GitHub repository.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+---
+
+Happy coding! 🚀
---
+++ index.ts
@@ -0,0 +1,590 @@
+import axios, { Method } from "axios";
+
+/**
+ * @constant baseUrl
+ * @description The base URL of the Tebex Headless API
+ * 
+ * @type {string}
+ */
+const baseUrl: string = "https://headless.tebex.io";
+
+/**
+ * @var webstoreIdentifier
+ * @description The identifier of the webstore
+ * 
+ * @type {string | null}
+ */
+export let webstoreIdentifier: string | null = null;
+
+/**
+ * @function SetWebstoreIdentifier
+ * @description A function to set the webstore identifier
+ * 
+ * @param {string} identifier The identifier of the webstore
+ * 
+ * @returns {void}
+ */
+export function SetWebstoreIdentifier(identifier: string): void {
+    webstoreIdentifier = identifier;
+}
+
+/**
+ * @interface Data
+ * @description The data returned from the Tebex Headless API
+ * 
+ * @type {T} The type of data to be returned
+ */
+export interface Data<T> {
+    data: T
+}
+
+/**
+ * @interface Message
+ * @description The message returned from the Tebex Headless API
+ * 
+ * @param {string} message The message returned from the Tebex Headless API
+ */
+export interface Message {
+    message: string
+}
+
+/**
+ * @type {GenericObject}
+ * @description A generic object
+ */
+export type GenericObject = string | number | boolean | null | undefined;
+
+/**
+ * @type {Route}
+ * @description The route of the Tebex Headless API
+ * 
+ * @param {string} accounts The accounts route
+ * @param {string} baskets The baskets route
+ */
+export type Route = "accounts" | "baskets";
+
+/**
+ * @type {ApplyType}
+ * @description The type of the apply request
+ * 
+ * @param {string} coupons The coupons type
+ * @param {string} giftcards The giftcards type
+ * @param {string} creator-codes The creator codes type
+ */
+export type ApplyType = "coupons" | "giftcards" | "creator-codes";
+
+/**
+ * @type {KeyValuePair}
+ * @description A key value pair
+ * 
+ * @type {K} The type of the key
+ * @type {V} The type of the value
+ */
+export type KeyValuePair<K extends string | number, V> = {
+    [key in K]: V;
+}
+
+/**
+ * @interface BaseItem
+ * @description The base item object for the package and category objects
+ * 
+ * @param {number} id The ID of the base item
+ * @param {string} name The name of the base item
+ */
+export interface BaseItem {
+    id: number,
+    name: string
+}
+
+/**
+ * @interface CouponCodeBody
+ * @description The coupon code object for the body of the request
+ * 
+ * @param {string} coupon_code The coupon code to apply or remove
+ */
+export interface CouponCode {
+    coupon_code: string
+}
+
+/**
+ * @interface GiftCardCodeBody
+ * @description The gift card code object for the body of the request
+ * 
+ * @param {string} card_number The gift card code to apply or remove
+ */
+export interface GiftCardCode {
+    card_number: string
+}
+
+/**
+ * @interface CreatorCodeBody
+ * @description The creator code object for the body of the request
+ * 
+ * @param {string} creator_code The creator code to apply or remove
+ */
+export interface CreatorCode {
+    creator_code: string
+}
+
+/**
+ * @function Request
+ * @description A function to make a request to the Tebex Headless API
+ * 
+ * @param {Method | string} method The method of the request
+ * @param {Route} route The route of the request
+ * @param {string} path The path of the request
+ * @param {KeyValuePair<string, GenericObject>} params The parameters of the request
+ * 
+ * @returns {Promise<T>}
+ */
+export async function Request<T, Body>(method: Method | string, route: Route, path: string, params?: KeyValuePair<string, GenericObject>, body?: Body): Promise<T> {
+    let paramString = "";
+
+    if (params) {
+        paramString = Object.entries(params)
+            .filter(([_, value]) => value)
+            .map(([key, value]) => {
+                if (typeof value === "boolean") {
+                    value = value ? 1 : 0;
+                }
+
+                return [key, value].join("=")
+            })
+            .join("=");
+    }
+    
+    const response = await axios.request<T, T>({
+        url: `${baseUrl}/api/${route}/${webstoreIdentifier}${path}?${paramString}`,
+        method: method,
+        data: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    return response;
+}
+
+/**
+ * @interface Category
+ * @description The category object returned from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the category
+ * @param {string} name The name of the category
+ * @param {string} description The description of the category
+ * @param {Category | null} parent The parent category of the category
+ * @param {number} order The order of the category
+ * @param {Package[]} packages The packages in the category
+ * @param {"grid" | "list" | string} display_type The display type of the category
+ */
+export type Category = BaseItem & {
+    description: string,
+    parent: Category | null,
+    order: number,
+    packages: Package[],
+    display_type: "grid" | "list"
+}
+
+/**
+ * @function GetCategories
+ * @description A function to get the categories from the Tebex Headless API
+ * 
+ * @param {boolean} includePackages Whether to include the packages in the categories
+ * @param {string} basketIdent The identifier of the basket
+ * @param {string} ipAddress The IP address of the user
+ * 
+ * @returns {Promise<Data<Category[]>>}
+ */
+export async function GetCategories(includePackages?: boolean, basketIdent?: string, ipAddress?: string): Promise<Data<Category[]>> {
+    return await Request("get", "accounts", "/categories", {
+        includePackages,
+        basketIdent,
+        ipAddress 
+    });
+}
+
+/**
+ * @function GetCategory
+ * @description A function to get a category from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the category
+ * @param {boolean} includePackages Whether to include the packages in the category
+ * @param {string} basketIdent The identifier of the basket
+ * @param {string} ipAddress The IP address of the user
+ * 
+ * @returns {Promise<Data<Category>>}
+ */
+export async function GetCategory(id: number, includePackages?: boolean, basketIdent?: string, ipAddress?: string): Promise<Data<Category>> {
+    return await Request("get", "accounts", `/categories/${id}`, {
+        includePackages,
+        basketIdent,
+        ipAddress 
+    });
+}
+
+/**
+ * @function Apply
+ * @description A function to apply a coupon, giftcard or creator code to a basket
+ * 
+ * @param {T} t The body of the request
+ * @param {string} basketIdent The identifier of the basket
+ * @param {ApplyType} type The type of the apply request
+ * 
+ * @returns {Promise<Message>}
+ */
+export async function Apply<T>(t: T, basketIdent: string, type: ApplyType): Promise<Message> {
+    return await Request<Message, T>("post", "accounts", `/baskets/${basketIdent}/${type}`, {}, t);
+}
+
+/**
+ * @function Remove
+ * @description A function to remove a coupon, giftcard or creator code from a basket
+ * 
+ * @param {T} t The body of the request
+ * @param {string} basketIdent The identifier of the basket
+ * @param {ApplyType} type The type of the apply request
+ * 
+ * @returns {Promise<Message>}
+ */
+export async function Remove<T>(t: T, basketIdent: string, type: ApplyType): Promise<Message> {
+    return await Request<Message, T>("post", "accounts", `/baskets/${basketIdent}/${type}/remove`, {}, t);
+}
+
+/**
+ * @type {PackageType}
+ * @description The type of the package
+ * 
+ * @param {string} subscription The subscription type
+ * @param {string} single The single type
+ */
+export type PackageType = "subscription" | "single";
+
+/**
+ * @interface Package
+ * @description The package object returned from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the package
+ * @param {string} name The name of the package
+ * @param {string} description The description of the package
+ * @param {PackageType} type The type of the package
+ * @param {ShortCategory} category The category of the package
+ * @param {number} base_price The base price of the package
+ * @param {number} sales_tax The sales tax of the package
+ * @param {number} total_price The total price of the package
+ * @param {number} discount The discount of the package
+ * @param {string} created_at The date the package was created
+ * @param {string} updated_at The date the package was updated
+ */
+export type Package = BaseItem & {
+    description: string,
+    type: PackageType,
+    category: BaseItem,
+    base_price: number,
+    sales_tax: number,
+    total_price: number,
+    discount: number,
+    created_at: string,
+    updated_at: string
+}
+
+/**
+ * @function GetPackage
+ * @description A function to get a package from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the package
+ * @param {string} basketIdent The identifier of the basket
+ * @param {string} ipAddress The IP address of the user
+ * 
+ * @returns {Promise<Data<Package>>}
+ */
+export async function GetPackage(id: number, basketIdent?: string, ipAddress?: string): Promise<Data<Package>> {
+    return await Request("get", "accounts", `/packages/${id}`, {
+        basketIdent,
+        ipAddress 
+    });
+}
+
+/**
+ * @function GetPackages
+ * @description A function to get all packages from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * @param {string} ipAddress The IP address of the user
+ * 
+ * @returns {Promise<Data<Package[]>>}
+ */
+export async function GetPackages(basketIdent?: string, ipAddress?: string): Promise<Data<Package[]>> {
+    return await Request("get", "accounts", `/packages`, {
+        basketIdent,
+        ipAddress 
+    });
+}
+
+/**
+ * @interface InBasket
+ * @description The in_basket object inside a basket package object
+ * 
+ * @param {number} quantity The quantity of the package in the basket
+ * @param {number} price The price of the package in the basket
+ * @param {string | null} gift_username_id The ID of the user the package is gifted to
+ * @param {string | null} gift_username The username of the user the package is gifted to
+ */
+export interface InBasket {
+    quantity: number,
+    price: number,
+    gift_username_id: string | null,
+    gift_username: string | null
+}
+
+/**
+ * @interface BasketPackage
+ * @description The basket package object returned from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the package
+ * @param {string} name The name of the package
+ * @param {string} description The description of the package
+ * @param {InBasket} in_basket The in_basket object inside the basket package object
+ */
+export type BasketPackage = BaseItem & {
+    description: string,
+    in_basket: InBasket,
+}
+
+/**
+ * @interface Code
+ * @description The code object inside the basket coupons object
+ * 
+ * @param {string} code The code of the object
+ */
+export interface Code {
+    code: string
+}
+
+/**
+ * @interface Links
+ * @description The links object inside the basket object
+ * 
+ * @param {string} checkout The checkout link of the basket
+ */
+export interface Links {
+    checkout: string,
+    [key: string]: string
+}
+
+/**
+ * @interface Basket
+ * @description The basket object returned from the Tebex Headless API
+ * 
+ * @param {string} ident The identifier of the basket
+ * @param {boolean} complete Whether the basket is complete
+ * @param {number} id The ID of the basket
+ * @param {string} count The count of the basket
+ * @param {string} ip The IP address of the user
+ * @param {string | null} username_id The ID of the user
+ * @param {string | null} username The username of the user
+ * @param {number} base_price The base price of the basket
+ * @param {number} sales_tax The sales tax of the basket
+ * @param {number} total_price The total price of the basket
+ * @param {BasketPackage[]} packages The packages in the basket
+ * @param {Code[]} coupons The coupons in the basket
+ * @param {GiftCardCode[]} giftcards The giftcards in the basket
+ * @param {string} creator_code The creator code of the basket
+ * @param {Links} links The links of the basket
+ */
+export interface Basket {
+    ident: string,
+    complete: boolean,
+    id: number,
+    count: string,
+    ip: string,
+    username_id: string | null,
+    username: string | null,
+    base_price: number,
+    sales_tax: number,
+    total_price: number,
+    packages: BasketPackage[],
+    coupons: Code[],
+    giftcards: GiftCardCode[],
+    creator_code: string,
+    links: Links
+}
+
+/**
+ * @function GetBasket
+ * @description A function to get a basket from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * 
+ * @returns {Promise<Data<Package[]>>}
+ */
+export async function GetBasket(basketIdent: string): Promise<Data<Basket>> {
+    return await Request("get", "accounts", `/baskets/${basketIdent}`);
+}
+
+/**
+ * @type {Urls}
+ * @description The url object for the complete and cancel urls
+ * 
+ * @param {string} complete_url The complete url
+ * @param {string} cancel_url The cancel url
+ */
+export type Urls = {
+    complete_url: string,
+    cancel_url: string
+}
+
+/**
+ * @function CreateBasket
+ * @description A function to create a basket from the Tebex Headless API
+ * 
+ * @param {Urls} urls The urls object for the complete and cancel urls
+ * 
+ * @returns {Promise<Data<Basket>>}
+ */
+export async function CreateBasket(urls: Urls): Promise<Data<Basket>> {
+    return await Request<Data<Basket>, Urls>("post", "accounts", "/baskets", urls);
+}
+
+/**
+ * @interface AuthUrl
+ * @description The auth url object returned from the Tebex Headless API
+ * 
+ * @param {string} name The name of the auth url
+ * @param {string} url The url of the auth url
+ */
+export interface AuthUrl {
+    name: string,
+    url: string
+}
+
+/**
+ * @function GetBasketAuthUrl
+ * @description A function to get the auth url of a basket from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * @param {string} returnUrl The return url of the basket
+ * 
+ * @returns {Promise<AuthUrl[]>} The data returned or an axios error
+ */
+export async function GetBasketAuthUrl(basketIdent: string, returnUrl: string): Promise<AuthUrl[]> {
+    return await Request("get", "accounts", `/baskets/${basketIdent}/auth`, {}, {
+        returnUrl
+    });
+}
+
+/**
+ * @interface PackageBody
+ * @description The package object for the body of the request
+ * 
+ * @param {number} package_id The ID of the package
+ * @param {number} quantity The quantity of the package
+ * @param {PackageType} type The type of the package
+ */
+export interface PackageBody {
+    package_id: number,
+    quantity: number,
+    type: PackageType
+}
+
+/**
+ * @function AddPackageToBasket
+ * @description A function to add a package to a basket from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * @param {number} package_id The ID of the package
+ * @param {number} quantity The quantity of the package
+ * @param {PackageType} type The type of the package
+ * 
+ * @returns {Promise<Data<Package>>}
+ */
+export async function AddPackageToBasket(basketIdent: string, package_id: number, quantity: number, type: PackageType): Promise<Data<Package>> {
+    return await Request("post", "baskets", `/baskets/${basketIdent}/packages`, {}, {
+        package_id,
+        quantity,
+        type
+    });
+}
+
+/**
+ * @function GiftPackage
+ * @description A function to gift a package to a user from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * @param {number} package_id The ID of the package
+ * @param {string} target_username_id The ID of the user to gift the package to
+ * 
+ * @returns {Promise<Data<Package>>}
+ */
+export async function GiftPackage(basketIdent: string, package_id: number, target_username_id: string): Promise<Data<Package>> {
+    return await Request("post", "baskets", `/baskets/${basketIdent}/packages`, {}, {
+        package_id,
+        target_username_id
+    });
+}
+
+/**
+ * @function RemovePackage
+ * @description A function to remove a package from a basket from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * @param {number} package_id The ID of the package
+ * 
+ * @returns {Promise<Data<Package>>}
+ */
+export async function RemovePackage(basketIdent: string, package_id: number): Promise<Data<Package>> {
+    return await Request("post", "baskets", `/baskets/${basketIdent}/packages/remove`, {}, {
+        package_id
+    });
+}
+
+/**
+ * @function UpdateQuantity
+ * @description A function to update the quantity of a package in a basket from the Tebex Headless API
+ * 
+ * @param {string} basketIdent The identifier of the basket
+ * @param {number} package_id The ID of the package
+ * @param {number} quantity The quantity of the package
+ * 
+ * @returns {Promise<Data<Package>>}
+ */
+export async function UpdateQuantity(basketIdent: string, package_id: number, quantity: number): Promise<Data<Package>> {
+    return await Request("post", "baskets", `/baskets/${basketIdent}/packages/${package_id}`, {}, {
+        quantity
+    });
+}
+
+/**
+ * @interface Webstore
+ * @description The webstore object returned from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the webstore
+ * @param {string} description The description of the webstore
+ * @param {string} name The name of the webstore
+ * @param {string} webstore_url The webstore url of the webstore
+ * @param {string} currency The currency of the webstore
+ * @param {string} lang The language of the webstore
+ * @param {string} logo The logo of the webstore
+ * @param {string} platform_type The platform type of the webstore
+ * @param {string} created_at The date the webstore was created
+ */
+export interface Webstore {
+    id: number,
+    description: string,
+    name: string,
+    webstore_url: string,
+    currency: string,
+    lang: string,
+    logo: string,
+    platform_type: string,
+    created_at: string
+}
+
+/**
+ * @function GetWebstore
+ * @description A function to get the webstore from the Tebex Headless API
+ * 
+ * @returns {Promise<Data<Webstore>>}
+ */
+export async function GetWebstore(): Promise<Data<Webstore>> {
+    return await Request("get", "accounts", "");
+}
