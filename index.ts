8d097bce4d5cc47ec961d5979a067e2574e4503e
Tynopia <development@tynopia.net>
2024-10-16 23:05:51 +0200

    feat: Added Webstore Pages

--- index.ts
+++ index.ts
@@ -418,6 +418,36 @@ export type Webstore = {
     created_at: string;
 }
 
+/**
+ * @type {Page}
+ * @description The page object returned from the Tebex Headless API
+ * 
+ * @param {number} id The ID of the page
+ * @param {string} created_at The date the page was created
+ * @param {string} updated_at The date the page was updated
+ * @param {number} account_id The ID of the account
+ * @param {string} title The title of the page
+ * @param {string} slug The slug of the page
+ * @param {boolean} private Whether the page is private
+ * @param {boolean} hidden Whether the page is hidden
+ * @param {boolean} disabled Whether the page is disabled
+ * @param {boolean} sequence Whether the page is in a sequence
+ * @param {string} content The content of the page
+*/
+export type Page = {
+    id: number;
+    created_at: string;
+    updated_at: string;
+    account_id: number;
+    title: string;
+    slug: string;
+    private: boolean;
+    hidden: boolean;
+    disabled: boolean;
+    sequence: boolean;
+    content: string;
+}
+
 export class TebexHeadless {
     constructor(
         /**
@@ -893,6 +923,25 @@ export class TebexHeadless {
             this.webstoreIdentifier,
             "accounts"
         );
+        return data;
+    }
+
+    /**
+     * @function getPages
+     * @description A function to get the pages from the Tebex Headless API
+     * 
+     * @returns {Promise<Page>}
+    */
+    async getPages(): Promise<Array<Page>> {
+        const { data }: Data<Array<Page>> = await Request(
+            this.webstoreIdentifier,
+            this.privateKey,
+            "get",
+            this.webstoreIdentifier,
+            "accounts",
+            "/pages"
+        );
+
         return data;
     }
 
---
+++ tests/Pages.test.ts
@@ -0,0 +1,23 @@
+import { Page } from "..";
+
+const keys: Array<keyof Page> = [
+    "id",
+    "created_at",
+    "updated_at",
+    "account_id",
+    "title",
+    "slug",
+    "private",
+    "hidden",
+    "disabled",
+    "sequence",
+    "content"
+]
+
+test("testWebstorePagesStructure", async () => {
+    const pages = await global.tebexHeadless.getPages() as [Page];
+    const [ page ] = pages;
+
+    expect(pages.length).toBe(1)
+    expect(Object.keys(page).sort()).toEqual(keys.sort())
+})
